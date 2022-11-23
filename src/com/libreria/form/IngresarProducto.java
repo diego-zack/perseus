b6868e57834375c6ff59d55d3c2161dca910ef37
Fabian Diaz Torres <admin>
2022-11-22 22:28:19 -0300

    vistas2

--- src/com/libreria/form/IngresarProducto.java
+++ src/com/libreria/form/IngresarProducto.java
@@ -24,6 +24,8 @@ public class IngresarProducto extends javax.swing.JPanel {
         jLabel1 = new javax.swing.JLabel();
         PanelIngresoProducto = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
 
         setPreferredSize(new java.awt.Dimension(915, 600));
 
@@ -32,21 +34,45 @@ public class IngresarProducto extends javax.swing.JPanel {
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel1.setText("Ingresar Producto");
 
-        jLabel2.setText("HOOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+        PanelIngresoProducto.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+
+        jLabel3.setFont(new java.awt.Font("SansSerif", 0, 18)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(102, 102, 102));
+        jLabel3.setText("Nombre:");
+
+        jTextField1.setFont(new java.awt.Font("SansSerif", 0, 14)); // NOI18N
+        jTextField1.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        jTextField1.setText("Caja de Corchetes");
+        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout PanelIngresoProductoLayout = new javax.swing.GroupLayout(PanelIngresoProducto);
         PanelIngresoProducto.setLayout(PanelIngresoProductoLayout);
         PanelIngresoProductoLayout.setHorizontalGroup(
             PanelIngresoProductoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(PanelIngresoProductoLayout.createSequentialGroup()
-                .addGap(354, 354, 354)
-                .addComponent(jLabel2)
-                .addContainerGap(393, Short.MAX_VALUE))
+                .addGap(78, 78, 78)
+                .addComponent(jLabel3)
+                .addGroup(PanelIngresoProductoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(PanelIngresoProductoLayout.createSequentialGroup()
+                        .addGap(232, 232, 232)
+                        .addComponent(jLabel2))
+                    .addGroup(PanelIngresoProductoLayout.createSequentialGroup()
+                        .addGap(25, 25, 25)
+                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 519, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(81, Short.MAX_VALUE))
         );
         PanelIngresoProductoLayout.setVerticalGroup(
             PanelIngresoProductoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(PanelIngresoProductoLayout.createSequentialGroup()
-                .addGap(93, 93, 93)
+                .addGap(20, 20, 20)
+                .addGroup(PanelIngresoProductoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jTextField1))
+                .addGap(57, 57, 57)
                 .addComponent(jLabel2)
                 .addContainerGap(311, Short.MAX_VALUE))
         );
@@ -72,14 +98,20 @@ public class IngresarProducto extends javax.swing.JPanel {
                 .addComponent(jLabel1)
                 .addGap(56, 56, 56)
                 .addComponent(PanelIngresoProducto, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(36, Short.MAX_VALUE))
+                .addContainerGap(39, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField1ActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     public javax.swing.JPanel PanelIngresoProducto;
     public javax.swing.JLabel jLabel1;
     public javax.swing.JLabel jLabel2;
+    public javax.swing.JLabel jLabel3;
+    public javax.swing.JTextField jTextField1;
     // End of variables declaration//GEN-END:variables
 }
